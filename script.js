7d3204bcaa66dd72f859cbd48ee29e018398b344
mdAlnahian <85539322+mdAlnahian@users.noreply.github.com>
2021-08-11 09:53:22 +0600

    website practice uploaded

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+var nahian = document.getElementById("menubtn");
+var sidenav = document.getElementById("sidenav");
+
+    sidenav.style.right = "-250px";
+    menubtn.onclick = function() {
+        if (sidenav.style.right == "-250px") {
+                sidenav.style.right = "0";
+            }
+        else{
+            sidenav.style.right = "-250px";
+        }
+    }
+*/
+    
